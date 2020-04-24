ab41fcc9c58e3e083bce4a5c133b52baad1e0222
ljurak <lucas.jurak@gmail.com>
2020-04-24 23:50:10 +0200

    Modify task to search for a list of patterns

--- src/main/java/file/CheckFileTask.java
+++ src/main/java/file/CheckFileTask.java
@@ -2,6 +2,7 @@ package file;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 public class CheckFileTask implements Callable<String> {
@@ -10,21 +11,15 @@ public class CheckFileTask implements Callable<String> {
 
     private final PatternSearchEngine searchEngine;
 
-    private final byte[] pattern;
-
-    private final String message;
-
-    public CheckFileTask(File file, PatternSearchEngine searchEngine, byte[] pattern, String message) {
+    public CheckFileTask(File file, PatternSearchEngine searchEngine) {
         this.file = file;
         this.searchEngine = searchEngine;
-        this.pattern = pattern;
-        this.message = message;
     }
 
     @Override
     public String call() throws Exception {
         byte[] fileBytes = Files.readAllBytes(file.toPath());
-        boolean searchResult = searchEngine.kmpSearch(fileBytes, pattern);
-        return file.getName() + ": " + (searchResult ? message : "Unknown file type");
+        List<Pattern> searchResult = searchEngine.searchPatterns(fileBytes);
+        return file.getName() + ": " + (!searchResult.isEmpty() ? searchResult : "Unknown file type");
     }
 }
